8acd0a82c246a3f2c5efa8c1236745b8ff589bdd
Marine Belin <marinebelin.39@gmail.com>
2017-04-07 11:19:37 +0200

    test menu toujours pas reussi

--- js/tooglemenu.js
+++ js/tooglemenu.js
@@ -1,4 +1,4 @@
-$( ".buttonmenu" ).click(function() {
+document.getElementById( "buttonmenu" ).click(function() {
   if(parseInt($(".testmenu").css("left")) == -300)  
   $(".testmenu").stop().animate({left: "+=300px"}, 500);  
   if(parseInt($(".testmenu").css("left")) >= 0)  
